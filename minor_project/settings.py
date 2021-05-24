76c50de52f60b50703b9922f0c36af7f0ba08ba2
sub0101 <surajbisht0101@gmail.com>
2021-05-24 10:58:46 +0530

    chnage

--- minor_project/settings.py
+++ minor_project/settings.py
@@ -112,7 +112,12 @@ DATABASES = {
 #         'PORT': '',
 #     }
 # }
-DATABASES = { 'default' : dj_database_url.config()}
+# DATABASES = { 'default' : dj_database_url.config()}
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
+}
 
 
 
