66251cff3d2da54b93e8980d8ef84a9fc13f4977
sub0101 <surajbisht0101@gmail.com>
2021-05-25 13:18:14 +0530

    a

--- minor_project/settings.py
+++ minor_project/settings.py
@@ -11,8 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 from django.contrib.messages import constants as messages
 import os
-import cloudinary
-import cloudinary_storage
+
 
 import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
