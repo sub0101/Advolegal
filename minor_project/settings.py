1a61024e3b7df08a2f45ab99a6ddc213732853b5
sub0101 <surajbisht0101@gmail.com>
2021-05-30 01:53:14 +0530

    a

--- minor_project/settings.py
+++ minor_project/settings.py
@@ -153,9 +153,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
 MEDIA_URL = '/media/'
-
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR , 'static'),
+)
 # message framework django
 
 MESSAGE_TAGS = {
