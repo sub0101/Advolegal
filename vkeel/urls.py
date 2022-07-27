c51bb69803be95ef74109007fc3cb43aabfb7c75
suraj bisht <surajbisht0101@gmail.com>
2022-07-28 01:02:25 +0530

    'a'

--- vkeel/urls.py
+++ vkeel/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from .views import *
 from django.contrib.auth import views as auth_views
+
 urlpatterns = [
 
     path( '' , index , name = 'index' ),
