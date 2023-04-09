d254a9385be9991bd7dc9e010076de8ba7bf9605
sub0101 <surajbisht0101@gmail.com>
2023-04-10 01:47:07 +0530

    commit for college prject

--- minor_project/settings.py
+++ minor_project/settings.py
@@ -99,12 +99,12 @@ WSGI_APPLICATION = 'minor_project.wsgi.application'
 # # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -115,6 +115,20 @@ DATABASES = {
 #         'PORT': '',
 #     }
 # }
+
+DATABASES = {  
+    'default': {  
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'advolegal',  
+        'USER': 'root',  
+        'PASSWORD': 'suraj01',  
+        'HOST': '127.0.0.1',  
+        'PORT': '3306',  
+        'OPTIONS': {  
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
+        }  
+    }  
+}  
 
 
 
